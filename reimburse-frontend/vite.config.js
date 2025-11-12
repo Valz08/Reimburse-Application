d9230bcbed684c3aaef57707f47ea4cc108bc33f
Valz08 <revalvarel08@gmail.com>
2025-11-12 15:00:04 +0700

    chore: add allowedHosts for CRC domain

--- reimburse-frontend/vite.config.js
+++ reimburse-frontend/vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,         // agar bisa diakses dari IP lokal
-    port: 3000          // bebas, asal tahu port-nya
+    port: 3000          // port lokal kamu tetap aman
+  },
+  preview: {
+    port: 8080,         // port runtime di OpenShift
+    allowedHosts: [
+      'reimburse-frontend-webapp.apps-crc.testing' // ✅ whitelist domain CRC
+    ]
   }
 })
